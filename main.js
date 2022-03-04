73d9e28cd8963bd590880b600938494f1f1b073b
Ibrahims168 <96667811+Ibrahims168@users.noreply.github.com>
2022-03-04 03:43:56 +0200

    adding a like btn

--- main.js
+++ main.js
@@ -33,20 +33,36 @@ const body = document.getElementsByTagName('body')[0]
 const createPost = function () {
     const text = input.value
     const img = document.createElement('img')
-    const post = document.createElement('h1')
+    const post = document.createElement('h3')
     const hr = document.createElement('hr')
-    const name = document.createElement('h2')
-    const comment = document.createElement('input')
-    comment.innerHTML = input
-    comment.style.
+    const name = document.createElement('h3')
+    const like = document.createElement("h1")
+
+    like.innerText = "like"
+    like.style.color = "gray"
+    like.style.marginLeft = "30%"
+
+    like.addEventListener('click', function () {
+        if (like.style.color == 'gray') {
+            like.style.color = "pink"
+        }else if(like.style.color == "pink"){
+            like.style.color = "gray"
+        }
+    })
+
     post.innerText = text
+    post.style.marginTop = "40px"
+    post.style.marginLeft = "8%"
+
     name.innerText = "Ibrahim Sharif"
-    name.style.marginLeft = "10%"
-    name.style.marginTop = "-5%"
+    name.style.marginLeft = "8%"
+    name.style.marginTop = "-3%"
+
     img.style.width = "70px"
     img.style.borderRadius = "50%"
     img.src = 'images/nft2.jpg'
-    body.append(img,name,post,comment,hr)
+
+    body.append(img,name,post,like,hr)
     input.value = ''
 }
 btn.addEventListener('click', createPost)
